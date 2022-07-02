9d94a7b82d14ac0fa0893510569cecd21ad384b9
da3monE <daemonstrefall1011@gmail.com>
2022-07-02 17:55:05 -0400

    fixing build error 9

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,31 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-wordpress',
     options: {
-      "url": "https:/graphql"
+      "url": "https:/graphql",
+      baseUrl: process.env.API_URL,
+
+        protocol: process.env.API_PROTOCOL,
+
+        hostingWPCOM: false,
+
+        useACF: true,
+
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+          "**/menus",
+          "**/portfolio",
+          "**/services",
+          "**/qualifications",
+          "**/gallery",
+          "**/logo",
+          "**/location",
+        ],
     }
   }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
