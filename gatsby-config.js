7ce02116be7ce1f92fec147f6ebe0c42fa19eb40
da3monE <daemonstrefall1011@gmail.com>
2022-07-04 17:54:59 -0400

    fixing build error 13

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,18 @@
 /*process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"*/
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
     title: `ProjektEvil`,
-    siteUrl: `pjegatsbyjswordpressmain.gatsbyjs.io`
+    siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [{
     resolve: 'gatsby-source-wordpress',
     options: {
-      "url": "https://localhost/graphql"
+      "url": "https://localhost/graphql",
+      apiKey: process.env.API_KEY,
     }
   }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
